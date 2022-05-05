da4761f5ae9bf984776270bda319fd8e34060169
s-pat <sohanpatel96@gmail.com>
2022-05-05 10:25:07 -0400

    initial repo

---
+++ src/main/java/com/tiff/tfngatewayservice/TfnGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tiff.tfngatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfnGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfnGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiff/tfngatewayservice/TfnGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiff.tfngatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfnGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
